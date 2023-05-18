84a03d44e7976ac3350daed6a9d94917e5ad133d
mwongess <amosmwongelah@gmail.com>
2023-05-18 18:16:28 +0300

    Update LINK

--- src/BlogMain.jsx
+++ src/BlogMain.jsx
@@ -3,27 +3,28 @@ import { Link } from "react-router-dom";
 /* eslint-disable react/prop-types */
 export const BlogMain = ({ blogs }) => {
   return (
-    <Link to="/blogs/details">
-      <div>
-        {blogs.map((blog) => (
-          <div className="left">
-            <div className="top">
-              <div>
-                <img
-                  src="https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small/default-avatar-profile-icon-of-social-media-user-vector.jpg"
-                  alt=""
-                />
-              </div>
-              <h4 className="title">{blog.author}</h4>
+    <div>
+      {blogs.map((blog) => (
+        <div className="left">
+          <div className="top">
+            <div>
+              <img
+                src="https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small/default-avatar-profile-icon-of-social-media-user-vector.jpg"
+                alt=""
+              />
             </div>
+            <h4 className="title">{blog.author}</h4>
+          </div>
+          <Link to="/blogs/details">
             <div className="blog">
               <h4>{blog.blogName}</h4>
               <p>{blog.description}</p>
             </div>
-            <hr />
-          </div>
-        ))}
-      </div>
-    </Link>
+          </Link>
+
+          <hr />
+        </div>
+      ))}
+    </div>
   );
 };
