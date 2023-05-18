a87d226a32a6404fb8f395c03790ab9046120e26
mwongess <amosmwongelah@gmail.com>
2023-05-18 17:09:38 +0300

    Update repo

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,9 +1,39 @@
-import { BlogHeader } from "./BlogHeader"
+import { BlogHeader } from "./BlogHeader";
 
-export const BlogDetails = ({data}) => {
-    return (
+export const BlogDetails = ({ data }) => {
+  return (
+    <div className="details">
+      <div>
         <div>
-
+            <img src="https://react.dev/images/og-home.png" alt="" />
         </div>
-    )
-}
+        <h3>
+          Title: Handling the "Cannot destructure property 'basename' of
+          'React2.useContext(...)' as it is null" Error in React
+        </h3>
+        <div>
+          Introduction: When working with React applications, it's not uncommon
+          to encounter errors while using hooks like useContext. One such error
+          is the "Cannot destructure property 'basename' of
+          'React2.useContext(...)' as it is null" error. This error typically
+          occurs when attempting to destructure a property from the result of
+          the useContext hook, but the value being destructured is null. In this
+          blog post, we will explore the possible causes of this error and
+          discuss how to resolve it effectively. I. Understanding the Error: The
+          error message indicates that the 'basename' property cannot be
+          destructured from the result of the useContext hook because it is
+          null. To troubleshoot and fix this error, we need to examine the
+          context initialization, usage, and potential null values. II. Causes
+          of the Error: Uninitialized or Incorrect Context Provider: Check if
+          the context provider is properly initialized and available in the
+          component tree. Ensure that the context provider is providing the
+          expected values, including the 'basename' property. Incorrect Context
+          Usage: Verify that you are correctly importing the context object and
+          using the useContext hook. Double-check that the useContext hook is
+          called with the correct context object. Make sure the context object
+          contains the 'basename' property.
+        </div>
+      </div>
+    </div>
+  );
+};
